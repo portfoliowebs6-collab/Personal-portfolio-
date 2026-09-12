fc6f769de7b08eb65401306e6533020f59df7504
portfoliowebs6-collab <portfoliowebs6@gmail.com>
2026-09-12 21:04:29 +0530

    Delete backend/server.js

--- backend/server.js
+++
@@ -1,4 +0,0 @@
-// Server.js me routes ko uncomment karke aise add karein:
-app.use('/api/projects', require('./routes/projectRoutes'));
-app.use('/api/messages', require('./routes/messageRoutes'));
-app.use('/api/auth', require('./routes/authRoutes'));
