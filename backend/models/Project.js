7a61e18233eb57134ecbd105bc8344a4fd80de81
portfoliowebs6-collab <portfoliowebs6@gmail.com>
2026-09-12 20:45:25 +0530

    Add files via upload

---
+++ backend/models/Project.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const projectSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  techStack: [String],
+  liveUrl: String,
+  githubUrl: String,
+  image: String
+}, { timestamps: true });
+
+module.exports = mongoose.model('Project', projectSchema);
